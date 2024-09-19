0858f9c634c6e21759ccdef151942004d25290c5
Callum Ilett(Front-End Developer) <cilett99@gmail.com>
2024-09-19 21:19:24 +0100

    🔧 Add global logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,64 @@
+/* eslint-env node */
+import { consoleTransport, logger } from "react-native-logs";
+
+// ###################
+// The level you want to filter out in the console and not print
+const SEVERITY = "debug";
+// ###################
+
+// ###################
+// Set to false to disable all logs
+const isJest = typeof jest !== "undefined";
+export const LOGGING_ENABLED = !isJest && __DEV__;
+// ###################
+
+// ###################
+// LOGGING_EXTENSIONS
+const LOGGING_EXTENSIONS = {};
+//  ###################
+
+// ###################
+// Add the extensions you want to enable here or set to an empty array to enable all
+//  ###################
+const ENABLED_EXTENSIONS: never[] | string[] = [];
+
+type LoggingLevels = "debug" | "info" | "warn" | "error";
+
+const loggerInstance = logger.createLogger<LoggingLevels>({
+	levels: {
+		debug: 0,
+		info: 1,
+		warn: 2,
+		error: 3
+	},
+	severity: SEVERITY,
+	transport: consoleTransport,
+	transportOptions: {
+		colors: {
+			info: "blueBright",
+			warn: "yellowBright",
+			error: "redBright"
+		}
+	},
+	enabled: LOGGING_ENABLED,
+	enabledExtensions: ENABLED_EXTENSIONS,
+	async: true,
+	dateFormat: "time",
+	printLevel: true,
+	printDate: true,
+	fixedExtLvlLength: false
+});
+
+const start = () => {
+	console.log("███████╗████████╗ █████╗ ██████╗ ████████╗");
+	console.log("██╔════╝╚══██╔══╝██╔══██╗██╔══██╗╚══██╔══╝");
+	console.log("███████╗   ██║   ███████║██████╔╝   ██║   ");
+	console.log("╚════██║   ██║   ██╔══██║██╔══██╗   ██║   ");
+	console.log("███████║   ██║   ██║  ██║██║  ██║   ██║   ");
+	console.log("╚══════╝   ╚═╝   ╚═╝  ╚═╝╚═╝  ╚═╝   ╚═╝   ");
+};
+
+export const logging = {
+	...loggerInstance,
+	start
+};
