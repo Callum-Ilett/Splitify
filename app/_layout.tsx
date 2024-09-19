165b8be833dcf89cd5f059100b0f52d241fa4f5a
Callum Ilett(Front-End Developer) <cilett99@gmail.com>
2024-09-19 20:42:20 +0100

    🔧 Add configuration for nativewind/tailwindcss

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,10 @@
+import "../global.css";
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return (
-    <Stack>
-      <Stack.Screen name="index" />
-    </Stack>
-  );
+	return (
+		<Stack>
+			<Stack.Screen name="index" />
+		</Stack>
+	);
 }
